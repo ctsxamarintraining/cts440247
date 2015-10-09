b06101ed3c2e91f7a7f9fdad7fe80c321108868d
cts440247 <shylaja.r@cognizant.com>
2015-10-09 17:42:15 +0530

    Garbage collection assignment

---
+++ AssignmentGarbageCollection/AssignmentGarbageCollection/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AssignmentGarbageCollection
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			long totalMemoryAvailable = GC.GetTotalMemory (true);
+			Console.WriteLine("Total memory before reserving: " + totalMemoryAvailable);
+			long memorySpaceAvble1 = GC.GetTotalMemory (false);
+			{
+				//Initialize an array and make it unreachable
+				string[] unreachableArray = new string[totalMemoryAvailable];
+				unreachableArray = null;
+			}
+			Console.WriteLine("Total memory after reserving unreachable array: " + memorySpaceAvble1);
+			GC.Collect();
+			Console.WriteLine("Total memory after freeing: " + GC.GetTotalMemory(true));
+
+		}
+	}
+}
